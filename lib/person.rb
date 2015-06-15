cbdf06692c45ebcc842f95d2e9f303473951edc5
Lia Skalkos <hello@liaskalkos.net>
2015-06-15 15:36:57 -0400

    Cool metaprogramming complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+
 class Person
   # code here
+
+  def initialize(options={})
+
+    options.each do |property, value|
+      instance_variable_set("@#{property}", value)
+      self.define_singleton_method(property){ instance_variable_get("@#{property}") }
+    end
+
+  end
+
 end
